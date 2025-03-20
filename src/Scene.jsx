f1a4b1d20493897c7e23c3d2aa7f2cf23e056c9e
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2025-03-21 03:10:51 +0530

    added tree glb file and loaded asset

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,38 +1,19 @@
 import { useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
-
-function Box(props) {
-  const mesh = useRef();
-  const [hovered, setHovered] = useState(false);
-  const [active, setActive] = useState(false);
-
-  useFrame((state, delta) => {
-    mesh.current.rotation.x += delta;
-  });
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      castShadow
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHovered(true)}
-      onPointerOut={(event) => setHovered(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-}
+import { Trees } from "./Trees";
 
 export const Scene = () => {
   return (
     <>
       <ambientLight intensity={0.1} />
-      <pointLight position={[10, 10, 10]} castShadow />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <directionalLight
+        color="white"
+        position={[15, 15, 15]}
+        castShadow
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+      />
+      <Trees />
     </>
   );
 };
---
+++ src/Trees.jsx
@@ -0,0 +1,20 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export function Trees(props) {
+  const { nodes, materials } = useGLTF("/trees.glb");
+  console.log(nodes);
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Foliage.geometry}
+        material={materials["Stylized Foliage"]}
+        position={[0.33, -0.05, -0.68]}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/trees.glb");
