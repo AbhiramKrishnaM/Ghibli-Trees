3d2af08c6a4a42fce58967d7e33cc6ee6ee150d3
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2025-03-21 03:41:24 +0530

    added toon shader

--- src/Trees.jsx
+++ src/Trees.jsx
@@ -1,20 +1,51 @@
-import React, { useRef } from "react";
+import { useMemo, forwardRef } from "react";
 import { useGLTF } from "@react-three/drei";
+import { ToonShader } from "./ToonShader";
+import { Color, Vector3 } from "three";
 
-export function Trees(props) {
-  const { nodes, materials } = useGLTF("/trees.glb");
-  console.log(nodes);
+export const Trees = forwardRef((props, ref) => {
+  const { nodes } = useGLTF("/trees.glb");
+
+  const uniforms = useMemo(() => {
+    return {
+      ...ToonShader.uniforms,
+      uBaseColor: {
+        value: new Color("#49897c"),
+      },
+      uAmbientLightColor: {
+        value: new Color("#050505"),
+      },
+      uDirLightPos: {
+        value: new Vector3(15, 15, 15),
+      },
+      uDirLightColor: {
+        value: new Color("white"),
+      },
+      uLineColor1: {
+        value: new Color("#808080"),
+      },
+      uLineColor2: {
+        value: new Color("#000000"),
+      },
+    };
+  }, []);
+
   return (
-    <group {...props} dispose={null}>
+    <group ref={ref} {...props} dispose={null}>
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Foliage.geometry}
-        material={materials["Stylized Foliage"]}
         position={[0.33, -0.05, -0.68]}
-      />
+      >
+        <shaderMaterial
+          attatch="material"
+          {...ToonShader}
+          uniforms={uniforms}
+        />
+      </mesh>
     </group>
   );
-}
+});
 
 useGLTF.preload("/trees.glb");
