28b8938d3c5d7dbde6aa26c50da7679b183bbacc
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2025-03-21 02:56:34 +0530

    added shadow

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -3,10 +3,25 @@ import { useFrame } from "@react-three/fiber";
 
 function Box(props) {
   const mesh = useRef();
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame((state, delta) => {
+    mesh.current.rotation.x += delta;
+  });
+
   return (
-    <mesh {...props}>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      castShadow
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHovered(true)}
+      onPointerOut={(event) => setHovered(false)}
+    >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={"orange"} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
     </mesh>
   );
 }
@@ -14,8 +29,8 @@ function Box(props) {
 export const Scene = () => {
   return (
     <>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      <ambientLight intensity={0.1} />
+      <pointLight position={[10, 10, 10]} castShadow />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
     </>
